bc67ea8d781a587fe49b7b55e2851a36493e7f15
mouna <mounaettalbi@gmail.com>
2024-10-28 21:47:37 +0100

    add CRUD implemantatin

--- Book.ts
+++ Book.ts
@@ -1,15 +1,15 @@
 export default class Book {
-    title: string;
-    author: string;
-    pages: number;
-    pagesRead: number;
-    status: string;
-    format: string;
-    suggestedBy: string;
-    price: number;
-    finished: boolean;
-  
-    constructor(
+  title: string;
+  author: string;
+  pages: number;
+  pagesRead: number;
+  status: string;
+  format: string;
+  suggestedBy: string;
+  price: number;
+  finished: boolean;
+
+  constructor(
       title: string,
       author: string,
       pages: number,
@@ -18,8 +18,7 @@ export default class Book {
       pagesRead: number,
       format: string,
       suggestedBy: string,
-      
-    ) {
+  ) {
       this.title = title;
       this.author = author;
       this.pages = pages;
@@ -29,14 +28,47 @@ export default class Book {
       this.suggestedBy = suggestedBy;
       this.price = price;
       this.finished = pagesRead >= pages;
-    }
-  
-    currentlyAt(): number {
-      return (this.pagesRead / this.pages) * 100;
-    }
-  
-    deleteBook(books: Book[]): Book[] {
-      return books.filter(book => book.title !== this.title);
-    }
   }
-  
+
+  currentlyAt(): number {
+      return (this.pagesRead / this.pages) * 100;
+  }
+
+  updateBook(details: Partial<Omit<Book, 'title' | 'finished'>>): void {
+      Object.assign(this, details);
+      this.finished = this.pagesRead >= this.pages; // Update finished status
+  }
+}
+
+class BookManager {
+  private books: Book[] = [];
+
+  // Create
+  addBook(book: Book): void {
+      this.books.push(book);
+  }
+
+  // Read
+  getBooks(): Book[] {
+      return this.books;
+  }
+
+  getBook(title: string): Book | undefined {
+      return this.books.find(book => book.title === title);
+  }
+
+  // Update
+  updateBook(title: string, updatedDetails: Partial<Omit<Book, 'title' | 'finished'>>): boolean {
+      const book = this.getBook(title);
+      if (book) {
+          book.updateBook(updatedDetails);
+          return true; // Update was successful
+      }
+      return false; // Book not found
+  }
+
+  // Delete
+  deleteBook(title: string): void {
+      this.books = this.books.filter(book => book.title !== title);
+  }
+}
