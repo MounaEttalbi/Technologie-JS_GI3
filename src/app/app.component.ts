cd7a5b532a2181bca739ff9cd52cd0241f6005ce
mouna <mounaettalbi@gmail.com>
2024-12-02 15:57:30 +0100

    add too-way binding ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Book } from './shared/book.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Assurez-vous d'importer FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule], // Ajouter FormsModule ici
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
@@ -18,8 +19,8 @@ export class AppComponent {
     new Book('موسم الهجرة إلى الشمال', false),
     new Book('ذاكرة الجسد', true),
     new Book('The Great Gatsby', true),
-    new Book('Pride and Prejudice ', true)
-    
-
+    new Book('Pride and Prejudice', true)
   ];
+  
+  selectedBook: string = ''; // Propriété pour two-way binding
 }
