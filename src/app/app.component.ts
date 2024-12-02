6be8c8d4768726b979a98a8056a54a0330c4b8d2
mouna <mounaettalbi@gmail.com>
2024-12-02 15:36:17 +0100

    add book list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Book } from './shared/book.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Ajout de CommonModule ici
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Correction : styleUrls (pluriel)
 })
 export class AppComponent {
   title = 'tp-angular';
+
+  books: Book[] = [
+    new Book('ألف ليلة وليلة', true),
+    new Book('موسم الهجرة إلى الشمال', false),
+    new Book('ذاكرة الجسد', true)
+  ];
 }
